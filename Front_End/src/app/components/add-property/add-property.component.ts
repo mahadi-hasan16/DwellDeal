05f25ab77dcd570ba36ac5cf874a0e85f5bf765e
Md. Mahadi Hasan <mahadi.hasan.khan16@gmail.com>
2024-06-25 03:22:32 +0600

    Add Property

--- Front_End/src/app/components/add-property/add-property.component.ts
+++ Front_End/src/app/components/add-property/add-property.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-add-property',
   templateUrl: './add-property.component.html',
   styleUrls: ['./add-property.component.css'],
-  standalone: true
+  standalone: true,
+  imports:[
+    RouterLink,
+    RouterModule,
+    FormsModule
+  ]
 })
 export class AddPropertyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {
+  }
+
+  onBack()
+  {
+    this.router.navigate(['/']);
+  }
+
+  onSubmit(){
+    console.log('Submitted');
   }
 
 }
--- Front_End/src/app/components/property-details/property-details.component.ts
+++ Front_End/src/app/components/property-details/property-details.component.ts
@@ -1,16 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-property-details',
   templateUrl: './property-details.component.html',
   styleUrls: ['./property-details.component.css'],
+  imports: [
+    RouterLink
+  ],
   standalone: true
 })
 export class PropertyDetailsComponent implements OnInit {
+  public propertyId: number = 0;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
+    this.propertyId = Number(this.route.snapshot.params['id']);
+
+    this.route.params.subscribe((params) => {
+      this.propertyId = Number(params['id']);
+    });
   }
 
+  onSelectNext() {
+    this.propertyId += 1;
+    this.router.navigate(['app-property-details' + this.propertyId]);
+  }
+
+  onPaging(id:string){
+    this.router.navigate(['app-property-details/'+id]);
+  }
 }
